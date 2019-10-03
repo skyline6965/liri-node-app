dd27d9b8e6914fe369a877a33bf1a47f1d089e2f
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-03 15:15:26 -0500

    working on spotify-this-song, getting data, need to console.log

--- liri.js
+++ liri.js
@@ -1,34 +1,63 @@
 
 
 require("dotenv").config();
-var Spotify = require("node-spotify-api");
+
 var keys = require("./keys.js");
-var fs = require("fs");
-var axios = require("axios");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
 
-var userInput = process.argv.slice(3).join("%");
+var fs = require("fs");
+var axios = require("axios");
+var moment = require("moment");
+
+var command = process.argv[2];
+var userInput = process.argv.slice(3).join("+");
 var bandURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
 
 
-function searchApi(query) {
-    switch(query){
-        case 'concert-this':            
-            axios.get(bandURL)
-                .then(function (response) {
-                    console.log(bandURL);
-                })
+function searchApi() {
+    switch (command) {
+        case 'concert-this':
+            axios.get(bandURL).then(function (response) {
+                console.log(bandURL);
+                console.log(response.data[0].venue.name);
+                console.log(response.data[0].venue.city);
+                console.log(moment(response.data[0].datetime).format('MM/DD/YYYY'));
+
+            })
+                .catch(function (err) {
+
+                    console.log(err);
+                });
             break;
+
         case 'spotify-this-song':
-            axios.get('https://api.spotify.com/v1/search/q=' + userInput +  )
+            spotify.search({ type: 'track', query: userInput, limit:1}).then(function (response) {
+                // debugger;
+                // console.log(response);
+                // console.log(response.data[0].tracks);
+                var songObject = response.tracks.items;
+                for (var i = 0; i < songObject.length; i++) {
+                    console.log("Artist" + songObject[i].artists[i].name);
+                };
+
+
+
+            }).catch(function (err) {
+
+                console.log(err);
+
+            });
 
             break;
+
+
         default:
             console.log('test')
-            
+
             break;
     };
 };
 
-searchApi(process.argv[2]);
+searchApi();
