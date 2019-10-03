1bee751bf1d345300b00b448d9914dd81ab3673c
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-02 22:14:39 -0500

    writing main function that contains all argv

--- liri.js
+++ liri.js
@@ -1,8 +1,40 @@
 
 
 require("dotenv").config();
+var Spotify = require("node-spotify-api");
+var keys = require("./keys.js");
+var artist = process.argv[2].trim();
+var fs = require("fs");
+var axios = require("axios");
 
-var keys = requre("./keys.js");
+var spotify = new Spotify(keys.spotify);
 
-var spotify = new spotify(keys.spotify);
 
+
+
+var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+
+
+
+
+
+// function spotifyThisSong();
+// function movieThis();
+// function doWhatItSays();
+
+
+
+function searchApi(queryURL){
+    switch (queryURL) {
+        case 'concert-this':
+            axios.get('https://rest.bandsintown.com/artists/' + artist.slice(3).join("%"))
+                .then(function (response) {
+                    var concertData = response.data[0].venue.name;
+                    console.log(concertData);
+                })
+            break;
+    };
+};
+
+searchApi(process.argv[2]);
