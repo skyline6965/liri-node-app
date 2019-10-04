75a51018b6ead37b5f66b699dd6581f6b9bb0d09
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-03 23:15:16 -0500

    all main functions work

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Overview
 
-In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+In this assignment, I will make "LIRI". LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
 
 
 ### What Each Command Should Do
--- liri.js
+++ liri.js
@@ -13,18 +13,21 @@ var moment = require("moment");
 
 var command = process.argv[2];
 var userInput = process.argv.slice(3).join("+");
+
 if (!userInput) {
     userInput = "The+Sign";
 }
+
 var bandURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
-var movieURL = "http://www.omdbapi.com/?t=" + userInput +"&apikey=5f78b13";
+var movieURL = "http://www.omdbapi.com/?t=" + userInput + "&apikey=5f78b13";
 
 
 function searchApi() {
     switch (command) {
-        // Concert This-------------------------------------------------------------------------------------
+        // ----------------------------------Concert This------------------------------------------------
         case 'concert-this':
             axios.get(bandURL).then(function (response) {
+
                 console.log(bandURL);
                 console.log(response.data[0].venue.name);
                 console.log(response.data[0].venue.city);
@@ -36,26 +39,28 @@ function searchApi() {
                     console.log(err);
                 });
             break;
-        // Spotify this song -------------------------------------------------------------------------------------
+        // ----------------------------------Spotify this song -------------------------------------------------
         case 'spotify-this-song':
             spotify.search({ type: 'track', query: userInput, limit: 1 }).then(function (response) {
                 // debugger;
                 // console.log(JSON.stringify(response, null, 2));
-
                 var songObject = response.tracks.items;
                 for (var i = 0; i < songObject.length; i++) {
                     console.log("-----------------------------");
                     console.log("Artist: " + songObject[i].artists[i].name);
                     console.log("Song name: " + songObject[i].name);
                     console.log("Song Link: " + songObject[i].href);
-                    console.log("Song Link: " + songObject[i].album.name);
+                    console.log("Album Name: " + songObject[i].album.name);
                     console.log("-----------------------------");
                 };
             }).catch(function (err) {
                 console.log(err);
             });
+
             break;
-        // Movie This -------------------------------------------------------------------------------------
+
+        // ------------------------------------movie-this--------------------------------------------------------
+
         case 'movie-this':
             axios.get(movieURL).then(function (response) {
                 console.log("Title: " + response.data.Title);
@@ -68,33 +73,38 @@ function searchApi() {
                 console.log("Language: " + response.data.Language);
                 console.log("Plot: " + response.data.Plot);
                 console.log("Actors: " + response.data.Actors);
-
                 // console.log(movieURL);
-
-                // * Title of the movie.
-                // * Year the movie came out.
-                // * IMDB Rating of the movie.
-                // * Rotten Tomatoes Rating of the movie.
-                // * Country where the movie was produced.
-                // * Language of the movie.
-                // * Plot of the movie.
-                // * Actors in the movie.
-
-
-            })
-                .catch(function (err) {
-                    console.log(err);
-                });
+            }).catch(function (err) {
+                console.log(err);
+            });
 
             break;
-        // Concert This-------------------------------------------------------------------------------------
+        // --------------------------------------do-what-it-says-----------------------------------------------------
+        case 'do-what-it-says':
+            fs.readFile('random.txt', 'utf-8', function (err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+                var output = data.split(",");
+                for (var i = 0; i < output.length; i++) {
+                    console.log(output[i]);
 
+                }
+                // console.log(data);
+            });
 
-
+            break;
 
         default:
-            console.log('test')
-
+            console.log('-------------------------------------------------------')
+            console.log('Hello!');
+            console.log(' ');
+            console.log(' Please choose from one of the following commands:');
+            console.log(' ');
+            console.log(' > concert-this' + ' (i.e: concert-this Blink-182)');
+            console.log(' > spotify-this-song' + ' (i.e: spotify-this-song Welcome Home)');
+            console.log(' > movie-this' + ' (i.e: movie-this Blade Runner)');
+            console.log(' > do-what-it-says' + ' (i.e: dont do this one)');
             break;
     };
 };
